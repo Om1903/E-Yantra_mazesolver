d7b3bef1d31ca64f37aabd9e6d789c42a786b37a
Parekh Om Nirav <ee240002049@iiti.ac.in>
2025-10-05 19:15:15 +0530

    Create mux2.v

---
+++ srd/mux2.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+
+// mux2.v - logic for 2-to-1 multiplexer
+
+module mux2 #(parameter WIDTH = 8) (
+    input       [WIDTH-1:0] d0, d1,
+    input       sel,
+    output      [WIDTH-1:0] y
+);
+
+assign y = sel ? d1 : d0;
+
+endmodule
+
