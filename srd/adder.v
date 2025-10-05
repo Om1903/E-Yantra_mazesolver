9eef8ba62937c72c2a2a635686f2fe89b4da7e39
Parekh Om Nirav <ee240002049@iiti.ac.in>
2025-10-05 19:14:30 +0530

    Create adder.v

---
+++ srd/adder.v
@@ -0,0 +1,11 @@
+`timescale 1ns/1ps
+// adder.v - logic for adder
+
+module adder #(parameter WIDTH = 32) (
+    input       [WIDTH-1:0] a, b,
+    output      [WIDTH-1:0] sum
+);
+
+assign sum = a + b;
+
+endmodule
