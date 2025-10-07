9899300976b4a133e31af92b7355d44babaa3290
Parekh Om Nirav <ee240002049@iiti.ac.in>
2025-10-08 00:03:15 +0530

    Update riscv_cpu.v

--- srd/riscv_cpu.v
+++ srd/riscv_cpu.v
@@ -1,5 +1,4 @@
 `timescale 1ns/1ps
-
 module riscv_cpu (
     input        clk, reset,
     output [31:0] PC,
@@ -12,20 +11,26 @@ module riscv_cpu (
 );
 
     wire [1:0]  ResultSrc;
-    wire        PCSrc, ALUSrc, RegWrite, Jump, SrcASelect, Zero;
+    wire        PCSrc, ALUSrc, RegWrite, Jump, SrcASelect;
+    wire        Zero, LessThanS, LessThanU;
     wire [3:0]  ALUControl;
+    wire [2:0]  LoadType; // <-- NEW WIRE
 
     controller c (
-        .op(Instr[6:0]), .funct3(Instr[14:12]), .funct7b5(Instr[30]), .Zero(Zero),
+        .op(Instr[6:0]), .funct3(Instr[14:12]), .funct7b5(Instr[30]),
+        .Zero(Zero), .LessThanS(LessThanS), .LessThanU(LessThanU),
         .ResultSrc(ResultSrc), .MemWrite(MemWrite), .PCSrc(PCSrc), .ALUSrc(ALUSrc),
-        .RegWrite(RegWrite), .Jump(Jump), .SrcASelect(SrcASelect), .ALUControl(ALUControl)
+        .RegWrite(RegWrite), .Jump(Jump), .SrcASelect(SrcASelect), .ALUControl(ALUControl),
+        .LoadType(LoadType) 
     );
 
     datapath dp (
         .clk(clk), .reset(reset), .ResultSrc(ResultSrc), .PCSrc(PCSrc),
         .ALUSrc(ALUSrc), .RegWrite(RegWrite), .SrcASelect(SrcASelect),
-        .ALUControl(ALUControl), .Zero(Zero), .PC(PC), .Instr(Instr),
-        .Mem_WrAddr(Mem_WrAddr), .Mem_WrData(Mem_WrData), .ReadData(ReadData), .Result(Result)
+        .ALUControl(ALUControl), .LoadType(LoadType), 
+        .Zero(Zero), .LessThanS(LessThanS), .LessThanU(LessThanU),
+        .PC(PC), .Instr(Instr), .Mem_WrAddr(Mem_WrAddr), .Mem_WrData(Mem_WrData),
+        .ReadData(ReadData), .Result(Result)
     );
 
 endmodule
