9180536ec0cebf7f380e536f94c3cdc3ef04ce71
Parekh Om Nirav <ee240002049@iiti.ac.in>
2025-10-08 01:56:34 +0530

    Update riscv_cpu.v

--- srd/riscv_cpu.v
+++ srd/riscv_cpu.v
@@ -14,20 +14,21 @@ module riscv_cpu (
     wire        PCSrc, ALUSrc, RegWrite, Jump, SrcASelect;
     wire        Zero, LessThanS, LessThanU;
     wire [3:0]  ALUControl;
-    wire [2:0]  LoadType; // <-- NEW WIRE
+    wire [2:0]  LoadType; 
+    wire        is_jalr ;
 
     controller c (
         .op(Instr[6:0]), .funct3(Instr[14:12]), .funct7b5(Instr[30]),
         .Zero(Zero), .LessThanS(LessThanS), .LessThanU(LessThanU),
         .ResultSrc(ResultSrc), .MemWrite(MemWrite), .PCSrc(PCSrc), .ALUSrc(ALUSrc),
         .RegWrite(RegWrite), .Jump(Jump), .SrcASelect(SrcASelect), .ALUControl(ALUControl),
-        .LoadType(LoadType) 
+        .LoadType(LoadType) , .is_jalr(is_jalr)
     );
 
     datapath dp (
         .clk(clk), .reset(reset), .ResultSrc(ResultSrc), .PCSrc(PCSrc),
         .ALUSrc(ALUSrc), .RegWrite(RegWrite), .SrcASelect(SrcASelect),
-        .ALUControl(ALUControl), .LoadType(LoadType), 
+        .ALUControl(ALUControl), .LoadType(LoadType), .is_jalr(is_jalr),
         .Zero(Zero), .LessThanS(LessThanS), .LessThanU(LessThanU),
         .PC(PC), .Instr(Instr), .Mem_WrAddr(Mem_WrAddr), .Mem_WrData(Mem_WrData),
         .ReadData(ReadData), .Result(Result)
