4e0447ad40f5b17fc1c4cfdea5d6bf0ca5d12355
Parekh Om Nirav <ee240002049@iiti.ac.in>
2025-10-12 13:10:27 +0530

    Update instr_parser.v

--- srd/instr_parser.v
+++ srd/instr_parser.v
@@ -8,19 +8,19 @@
 //              in the higher-level modules.
 //////////////////////////////////////////////////////////////////////////////////
 module instr_parser (
-    input  [31:0] instr,      // The 32-bit instruction from memory
+    input  [31:0] instr,     
 
-    // Decoded fields as outputs
-    output [6:0]  op,         // Opcode field [6:0]
-    output [4:0]  rd,         // Destination register address [11:7]
-    output [2:0]  funct3,     // 3-bit function field [14:12]
-    output [4:0]  rs1,        // Source register 1 address [19:15]
-    output [4:0]  rs2,        // Source register 2 address [24:20]
-    output [6:0]  funct7,     // 7-bit function field [31:25]
+    
+    output [6:0]  op,         
+    output [4:0]  rd,         
+    output [2:0]  funct3,  
+    output [4:0]  rs1,        
+    output [4:0]  rs2,        
+    output [6:0]  funct7,     
 
     // Specific bits extracted for convenience in the decoders
-    output        funct7b5,   // Bit 5 of funct7 (instruction bit 30)
-    output        opb5        // Bit 5 of the opcode (instruction bit 5)
+    output        funct7b5,   
+    output        opb5      
 );
 
    
