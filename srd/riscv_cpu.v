3f25ebd73e3c02ebfe04cb0728687c9a63ec757a
Parekh Om Nirav <ee240002049@iiti.ac.in>
2025-10-12 12:37:01 +0530

    Update riscv_cpu.v

--- srd/riscv_cpu.v
+++ srd/riscv_cpu.v
@@ -1,12 +1,22 @@
 `timescale 1ns/1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Module Name: riscv_cpu
+// Description: This module represents the top-level CPU core. It integrates the
+//              'controller' (control path) and 'datapath' (data path) modules
+//              and defines the core's interface to external instruction and
+//              data memories.
+//////////////////////////////////////////////////////////////////////////////////
 module riscv_cpu (
+    // Core I/O
     input        clk, reset,
-    output [31:0] PC,
-    input  [31:0] Instr,
+
+    // Interface to Instruction Memory
+    output [31:0] PC,       
+    input  [31:0] Instr,  
     output        MemWrite,
     output [31:0] Mem_WrAddr,
     output [31:0] Mem_WrData,
-    input  [31:0] ReadData,
+    input  [31:0] ReadData,   
     output [31:0] Result
 );
 
@@ -14,17 +24,21 @@ module riscv_cpu (
     wire        PCSrc, ALUSrc, RegWrite, Jump, SrcASelect;
     wire        Zero, LessThanS, LessThanU;
     wire [3:0]  ALUControl;
-    wire [2:0]  LoadType; 
-    wire        is_jalr ;
+    wire [2:0]  LoadType;
+    wire [2:0]  BranchType;
+    wire        is_jalr;
 
+  
+    // The controller decodes the instruction and generates all control signals.
     controller c (
         .op(Instr[6:0]), .funct3(Instr[14:12]), .funct7b5(Instr[30]),
         .Zero(Zero), .LessThanS(LessThanS), .LessThanU(LessThanU),
         .ResultSrc(ResultSrc), .MemWrite(MemWrite), .PCSrc(PCSrc), .ALUSrc(ALUSrc),
         .RegWrite(RegWrite), .Jump(Jump), .SrcASelect(SrcASelect), .ALUControl(ALUControl),
-        .LoadType(LoadType) , .is_jalr(is_jalr)
+        .LoadType(LoadType), .BranchType(BranchType), .is_jalr(is_jalr)
     );
 
+    // The datapath contains the functional units (PC, RegFile, ALU) and executes the instruction based on the control signals from the controller.
     datapath dp (
         .clk(clk), .reset(reset), .ResultSrc(ResultSrc), .PCSrc(PCSrc),
         .ALUSrc(ALUSrc), .RegWrite(RegWrite), .SrcASelect(SrcASelect),
